8a7f1b40309ee4f70b917383fbdfe3cb21d3a928
Pedro Dias Soares <pedro3soares@gmail.com>
2025-08-12 07:13:08 -0300

    teste

---
+++ Java/Aula-1/helloword_semclasse.java
@@ -0,0 +1,7 @@
+public class helloword_semclasse{
+    
+    public static void main(String[]args){ // public static é o main || O Static é para eu poder usar em qualuqer lugar do codigo || "String[]args" =
+         System.out.println("Hello Word!");
+    }
+
+}
