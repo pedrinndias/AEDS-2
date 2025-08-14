07d8ed62ca4e3d6208bf1690618b7720fe75287b
Pedro Dias Soares <pedro3soares@gmail.com>
2025-08-14 09:47:21 -0300

    questao 10

--- C/Nivelamento/declaracao_de_variaveis_e_constantes/questao10.c
+++ C/Nivelamento/declaracao_de_variaveis_e_constantes/questao10.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
 #define TAXA_CAMBIO_DOLAR 5.25
 
-int mai(){
+int main(){
 
     int valor_reais=80;
+    float cambio=TAXA_CAMBIO_DOLAR*valor_reais;
 
-    printf("80 reais em dollar eh: %f",TAXA_CAMBIO_DOLAR*valor_reais );
+    printf("80 reais em dollar eh: %f", cambio );
 
     return 0;
 }
