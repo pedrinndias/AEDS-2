13facd2729e29154ddfd741806e38cc14ed09573
Pedro Dias Soares <pedro3soares@gmail.com>
2025-08-13 21:55:44 -0300

    questao1

---
+++ C/Nivelamento/declaracao_de_variaveis_e_constantes/questao1.c
@@ -0,0 +1,9 @@
+//Declare uma variável do tipo inteiro para armazenar a sua idade, atribua um valor a ela e imprima o valor na tela.
+#include<stdio.h>
+int main(){
+    int idade;
+    printf("Escreva sua idade: ");
+    scanf("%d", &idade);
+    printf("\n Sua idade: %d", idade);
+    return 0;
+}
