cf77cf74ace2e8ab622f4381247027d8dde98c5a
Pedro Dias Soares <pedro3soares@gmail.com>
2025-08-14 09:40:56 -0300

    questao 9

--- C/Nivelamento/declaracao_de_variaveis_e_constantes/questao9.c
+++ C/Nivelamento/declaracao_de_variaveis_e_constantes/questao9.c
@@ -5,10 +5,20 @@ int main(){
     float peso;
     char sang;
 
-    printf("Informe seua matricula, idade, peso e tipo sanguineo:");
-    scanf("%d%d%f%[^n]", &mat, &idade, &peso, &sang);
+    printf("Informe sua matricula:");
+    scanf("%d", &mat);
+    printf("Informe sua idade:");
+    scanf("%d", &idade);
+    printf("Informe seu peso:");
+    scanf("%f", &peso);
+    printf("Informe seu tipo sanguineo:");
+    scanf("%s", sang);
 
-    printf("Matricula")
+    printf("Matricula: %d \n", mat);
+    printf("idade: %d\n", idade);
+    printf("peso: %f\n", peso);
+    printf("Tipo sanguineo: %s", sang);
+
 
     return 0;
 }
