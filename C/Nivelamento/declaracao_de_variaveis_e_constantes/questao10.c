c1ca14372708c75abaf91f8919c9f589c912eb65
Pedro Dias Soares <pedro3soares@gmail.com>
2025-08-14 09:44:25 -0300

    teste

---
+++ C/Nivelamento/declaracao_de_variaveis_e_constantes/questao10.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#define TAXA_CAMBIO_DOLAR 5.25
+
+int mai(){
+
+    int valor_reais=80;
+
+    printf("80 reais em dollar eh: %f",TAXA_CAMBIO_DOLAR*valor_reais );
+
+    return 0;
+}
