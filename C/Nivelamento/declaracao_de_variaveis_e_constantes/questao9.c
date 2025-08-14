49fe784113fc8df32abd0c40434506f065403aa5
Pedro Dias Soares <pedro3soares@gmail.com>
2025-08-14 09:23:20 -0300

    questao 9

---
+++ C/Nivelamento/declaracao_de_variaveis_e_constantes/questao9.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+
+    int mat, idade;
+    float peso;
+    char sang;
+
+    printf("Informe seua matricula, idade, peso e tipo sanguineo:");
+    scanf("%d%d%f%[^n]", &mat, &idade, &peso, &sang);
+
+    printf("Matricula")
+
+    return 0;
+}
