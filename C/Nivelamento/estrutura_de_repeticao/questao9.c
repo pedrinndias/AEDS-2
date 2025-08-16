7762880ca51e0a13eb422b72fb363632190f4eb5
Pedro Dias Soares <pedro3soares@gmail.com>
2025-08-16 13:01:33 -0300

    questao 9

---
+++ C/Nivelamento/estrutura_de_repeticao/questao9.c
@@ -0,0 +1,2 @@
+// Escreva um programa que peça ao usuário um número N e gere os N primeiros termos da sequência de Fibonacci. A sequência começa com 0 e 1, e cada termo subsequente é a soma dos dois anteriores (0, 1, 1, 2, 3, 5, 8, ...).
+#include<stdio.h>
