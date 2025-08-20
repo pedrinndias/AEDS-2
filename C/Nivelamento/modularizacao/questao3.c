e0dfc4d5752a26b0d8801758304e35a3441b4b1c
Pedro Dias Soares <pedro3soares@gmail.com>
2025-08-19 22:20:38 -0300

    questao 3

---
+++ C/Nivelamento/modularizacao/questao3.c
@@ -0,0 +1,4 @@
+// Crie um procedimento chamado imprimirTabuada que recebe um número inteiro como parâmetro e 
+//imprime a tabuada desse número (de 1 a 10). A função não deve retornar nenhum valor.
+
+#include<stdio.h>
