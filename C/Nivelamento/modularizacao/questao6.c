db54ed45ddecc2719a4e452f928bfa8202f272f4
Pedro Dias Soares <pedro3soares@gmail.com>
2025-08-21 11:07:56 -0300

    q6

---
+++ C/Nivelamento/modularizacao/questao6.c
@@ -0,0 +1,25 @@
+// Crie uma função chamada maiorValor que recebe três números do tipo float como parâmetros e retorna o maior entre eles.
+
+
+#include<stdio.h>
+
+float maiorValor(float num1, float num2, float num3){
+    float maior;
+    if(num1>num2){
+        maior=num1;
+    }else{
+        maior=num2;
+    }
+
+    if(maior>num3){
+        return num3;
+    }else{
+        return maior;
+    }
+}
+
+int main(){
+    float num1, num2, num3;
+    printf
+    return 0;
+}
