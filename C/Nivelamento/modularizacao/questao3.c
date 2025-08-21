b6e832f66b43b4c027ed0f3ca2837797e2ca7153
Pedro Dias Soares <pedro3soares@gmail.com>
2025-08-21 10:11:57 -0300

    questao 3

--- C/Nivelamento/modularizacao/questao3.c
+++ C/Nivelamento/modularizacao/questao3.c
@@ -1,4 +1,17 @@
 // Crie um procedimento chamado imprimirTabuada que recebe um número inteiro como parâmetro e 
 //imprime a tabuada desse número (de 1 a 10). A função não deve retornar nenhum valor.
 
-#include<stdio.h>
+# include<stdio.h>
+void imprimirTabuada(int num){
+    for(int i=1; i<=10; i++){
+        printf("%d\n", num*i);
+    }
+}
+
+int main(){
+    int num;
+    printf("Escreva um numero: ");
+    scanf("%d", &num);
+    imprimirTabuada(num);
+    return 0;
+}
