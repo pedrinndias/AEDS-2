775683ffab8b950cb2c8b280c45a97083a4d2f46
Pedro Dias Soares <pedro3soares@gmail.com>
2025-08-25 08:07:40 -0300

    questao 1

--- C/Nivelamento/recursividade/questao1.c
+++ C/Nivelamento/recursividade/questao1.c
@@ -1,2 +1,20 @@
 // Crie uma função recursiva chamada fatorial que recebe um número inteiro não negativo n e retorna o seu fatorial. 
 // O caso base é quando n é 0, cujo fatorial é 1.
+
+#include<stdio.h>
+int fatorial(int num){
+    if(num==1){
+        return 1;
+    }else{
+        return num*fatorial(num-1);
+    }
+}
+int main(){
+
+    int num;
+    printf("Escreva um numero: ");
+    scanf("%d", &num);
+    printf("Fatorial: %d", fatorial(num));
+
+    return 0;
+}
