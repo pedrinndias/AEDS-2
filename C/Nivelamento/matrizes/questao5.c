eb7bc55550c58a04113070ffdb8f9cc7a5aec3a4
Pedro Dias Soares <pedro3soares@gmail.com>
2025-09-01 08:20:17 -0300

    questao 5

---
+++ C/Nivelamento/matrizes/questao5.c
@@ -0,0 +1,9 @@
+//Desenvolva um programa que leia uma matriz 3x4. 
+// Crie e imprima a matriz transposta (uma matriz 4x3 onde as linhas da original se tornam as colunas da nova).
+
+#include<stdio.h>
+#define LINHA 3
+#define LINHA 4
+int main(){
+
+}
