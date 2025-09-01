2d7c6fff33a4d6eaa61d550629d95a6947d844b6
Pedro Dias Soares <pedro3soares@gmail.com>
2025-09-01 09:27:47 -0300

    questao 5

--- C/Nivelamento/matrizes/questao5.c
+++ C/Nivelamento/matrizes/questao5.c
@@ -3,7 +3,35 @@
 
 #include<stdio.h>
 #define LINHA 3
-#define LINHA 4
+#define COLUNA 4
 int main(){
+     int matriz[LINHA][COLUNA], matrizT[COLUNA][LINHA];
+     for(int i=0;i<LINHA;i++){
+        for(int j=0;j<COLUNA;j++){
+            printf("Escreva um valor para matriz[%d][%d]: ", i+1, j+1);
+            scanf("%d", &matriz[i][j]);
+        }
+     }
+     printf("Matriz: \n");
+     for(int i=0;i<LINHA;i++){
+        for(int j=0;j<COLUNA;j++){
+            printf("[%d][%d]:%d ", i+1, j+1, matriz[i][j]);
+        }
+        printf("\n");
+     }
 
+     for(int i=0;i<COLUNA;i++){
+        for(int j=0;j<LINHA;j++){
+            matrizT[i][j]=matriz[j][i];
+        }
+     }
+     
+     printf("Matriz Transposta: \n");
+     for(int i=0;i<COLUNA;i++){
+        for(int j=0;j<LINHA;j++){
+            printf("[%d][%d]:%d ", i+1, j+1, matrizT[i][j]);
+        }
+        printf("\n");
+     }
+     return 0;
 }
