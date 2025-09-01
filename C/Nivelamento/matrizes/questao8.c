889e4263d95518aaae63dd563ca8490be6cfb6a1
Pedro Dias Soares <pedro3soares@gmail.com>
2025-09-01 10:27:13 -0300

    questao 8

---
+++ C/Nivelamento/matrizes/questao8.c
@@ -0,0 +1,18 @@
+// Faça um programa que leia uma matriz 3x3. 
+// Crie dois vetores: um para armazenar a soma de cada linha e outro para armazenar a soma de cada coluna. 
+// Imprima os dois vetores.
+
+#include<stdio.h>
+#define LINHA 3
+#define COLUNA 3
+int main(){
+    int vetL[LINHA], vetC[COLUNA], matriz[LINHA][COLUNA], auxL=0, auxC;
+    for(int i=0;i<LINHA;i++){
+        for(int j=0; j<COLUNA;j++){
+            printf("Escreva um valor para matriz[%d][%d]: ");
+            scanf("%d", &matriz[i][j]);
+            vetL[]
+        }
+    }
+    
+}
