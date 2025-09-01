c60b03bc89f0bc3c21f0238cda1c922f0217afea
Pedro Dias Soares <pedro3soares@gmail.com>
2025-09-01 15:58:45 -0300

    questao 10

---
+++ C/Nivelamento/matrizes/questao10.c
@@ -0,0 +1,3 @@
+// Este é um clássico. Crie um programa que leia duas matrizes, A (2x3) e B (3x2), e calcule a matriz produto C (que será 2x2). 
+// Imprima a matriz resultante. Lembrete: 
+// O elemento C[i][j] é a soma dos produtos dos elementos da linha i de A pelos elementos da coluna j de B.
