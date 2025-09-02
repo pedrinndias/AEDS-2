fbdbd375e1fc610e228f0294b32dc0d6ea94e0ea
Pedro Dias Soares <pedro3soares@gmail.com>
2025-09-02 14:44:53 -0300

    questao 8

--- C/Nivelamento/string/questao8.c
+++ C/Nivelamento/string/questao8.c
@@ -2,11 +2,19 @@
 // Por exemplo, se a entrada for "Eu gosto de programar", a saída deve ser "Eu gosto de programar".
 #include<stdio.h>
 #include<string.h>
+#include<stdlib.h>
 int main(){
     char str[150];
     printf("Escreva uma string: ");
     fgets(str,150,stdin);
-    
-
+    char *strLimpa=(char *)malloc(strlen(str)*sizeof(char));
+    int j=0;
+    for(int i=0;str[i]!='\0';i++){
+        if(str[i]!=' '){
+            strLimpa[j]=str[i];
+            j++;
+        }
+    }
+    printf("%s", strLimpa);
     return 0;
 }
