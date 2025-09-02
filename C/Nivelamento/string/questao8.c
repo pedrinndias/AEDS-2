6011854694184ef2e80966abb7afa43f07e07ab0
Pedro Dias Soares <pedro3soares@gmail.com>
2025-09-02 11:48:53 -0300

    questao 8

---
+++ C/Nivelamento/string/questao8.c
@@ -0,0 +1,12 @@
+// Faça um programa que leia uma frase e crie uma nova string removendo os espaços em branco extras entre as palavras. 
+// Por exemplo, se a entrada for "Eu gosto de programar", a saída deve ser "Eu gosto de programar".
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char str[150];
+    printf("Escreva uma string: ");
+    fgets(str,150,stdin);
+    
+
+    return 0;
+}
