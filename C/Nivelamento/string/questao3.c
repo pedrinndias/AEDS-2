f3e05f1ea915950a3807ac06028d13015ba54eda
Pedro Dias Soares <pedro3soares@gmail.com>
2025-09-02 10:12:08 -0300

    questao 4

--- C/Nivelamento/string/questao3.c
+++ C/Nivelamento/string/questao3.c
@@ -3,9 +3,28 @@
 // Desafio: implemente a lógica de concatenação manualmente, sem usar a função strcat().
 
 #include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 int main(){
-    int str1[150];
-    int str2[150];
-    int str3[300];
-    
+    char str1[150];
+    char str2[150];
+    int tam1, tam2;
+    printf("Escreva um string1 de ate 149 caracteres: ");
+    fgets(str1, sizeof(str1), stdin);
+    printf("Escreva um string2 de ate 149 caracteres: ");
+    fgets(str2 , sizeof(str2), stdin);
+    tam1=strlen(str1);
+    tam2=strlen(str2);
+    char *str3=(char *)malloc((tam1+tam2+1)*sizeof(char));
+    int aux=0;
+    int i,j;
+    for (i = 0; i < tam1; i++) {
+        str3[i] = str1[i];
+    }
+    for (j = 0; j < tam2; j++, i++) {
+        str3[i] = str2[j];
+    }
+    str3[tam1+tam2+1]='\0';
+    printf("%s", str3);
+    return 0;
 }
