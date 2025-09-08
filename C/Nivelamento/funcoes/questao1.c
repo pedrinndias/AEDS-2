4fe2d86d37828c979b1833e1d8746f59a9f2f162
Pedro Dias Soares <pedro3soares@gmail.com>
2025-09-08 07:15:39 -0300

    q1

---
+++ C/Nivelamento/funcoes/questao1.c
@@ -0,0 +1,27 @@
+//Crie uma função int somarVetor(int *vetor, int tamanho) que receba um ponteiro para o início de um vetor de inteiros e 
+//seu tamanho. Dentro da função, use um laço e aritmética de ponteiros (ex: *vetor e vetor++) para percorrer o vetor e somar 
+//todos os seus elementos. A função deve retornar a soma total.
+
+#include<stdio.h>
+#include<stdlib.h>
+int somarVetor(int *vetor, int tamanho){
+    if(tamanho==0){
+        return 0;
+    }else{
+        return vetor + somaVetor(vetor++,tamanho--);
+    }
+}
+int main(){
+    int tamanho;
+    printf("Informe o tamanho do vetor: ");
+    scanf("%d", &tamanho);
+    int *vetor=(int *)malloc(tamanho*sizeof(int));
+    if(vetor==NULL){
+        printf("Erro na alocacao dinamica de memoria!");
+        return 1;
+    }
+    for(int i=0;i<tamanho;i++){
+        printf("Preencha o vetor[%d]: ", i);
+        scanf("%d", &vetor[i]);
+    }
+}
